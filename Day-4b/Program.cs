269f3eae9629dc2fcf3e1bb78d5c077168a2e363
Michael Sanders <mike.gamedev@gmail.com>
2019-12-08 13:23:36 -0800

    Initial commit.

---
+++ Day-4b/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day_4b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> potentialPasswords = Enumerable.Range(108457, 453584);
+
+            var matchingPasswords = potentialPasswords
+                //Is a six digit number.
+                .Where(x => x >= 100000 && x < 1000000)
+                .Select(x => x.ToString().ToCharArray())
+                //Two adjacent digits are the same.
+                .Where(x =>
+                {
+                    for (int i = 0; i < x.Length - 1; i++)
+                        if (x[i] == x[i + 1])
+                            if (i - 1 < 0 || x[i - 1] != x[i])
+                                if (i + 2 >= x.Length || x[i + 2] != x[i])
+                                    return true;
+
+                    return false;
+                })
+                //Going from left to right, the digits never decrease
+                .Where(x =>
+                {
+                    for (int i = 1; i < x.Length; i++)
+                        if (int.Parse(x[i].ToString()) < int.Parse(x[i - 1].ToString()))
+                            return false;
+
+                    return true;
+                })
+                .Select(x => int.Parse(new string(x))).ToArray();
+
+            Console.WriteLine($"Found {matchingPasswords.Length} matching passwords.");
+        }
+    }
+}
